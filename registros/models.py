c77246743a75233288a92c78e3ba53ea943c898e
axelr <axelrepeat@gmail.com>
2025-10-03 07:18:13 -0600

    Definición de modelos y actualización del paquete django.

--- registros/models.py
+++ registros/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    division = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='logos_equipos/')
+
+    def __str__(self):
+        return self.nombre
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=100)
+    equipo  = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='archivos_jugadores/')
+    identifacion_pdf = models.FileField(upload_to='archivos_jugadores/')
+
+    def __str__(self):
+        return f'{self.nombre} - {self.equipo.nombre}'
